5dccf2020fd249b32734072e53162e05729eb41b
Ankita Chaturvedi <ankitachaturvedi503@gmail.com>
2024-05-19 12:14:56 +0530

    Deploy

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Programmer . . .", "Frontend Developer . . ." , "Web Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
